067577c72ed32afaf57e44254b12a0b53016fa85
William R. Fraser <wfraser@codewise.org>
2013-09-04 23:08:41 -0700

    copyright

--- Enums.cs
+++ Enums.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//
+// Copyright © William R. Fraser 2013
+//
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RpnTerm")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © William R. Fraser 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
